167346cfbb2e3b6882dab112525bba4f08ab9848
aldian <aldian.asmr@gmail.com>
2021-11-29 11:51:41 +0700

    quickfix

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import 'dotenv/config';
 import 'module-alias/register';
 
 import express from 'express';
-import { initalizeMongoDB, initializeExpress, initializeFirebaseAdmin } from './bootstrap';
+import { initializeExpress, initializeFirebaseAdmin } from './bootstrap';
 import { log } from './logger';
 
 const app = express();
 
 const start = async () => {
-  await Promise.all([initializeExpress(app), initalizeMongoDB(), initializeFirebaseAdmin()]);
+  await Promise.all([initializeExpress(app), initializeFirebaseAdmin()]);
 };
 
 start().then(() => log('all set!'));
